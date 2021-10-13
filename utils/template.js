8c45da258c97948d140c374e3c07f7063aab1c99
PapadakisGeorge <george.papadakis@camelotls.com>
2021-10-13 13:03:57 +0300

    Removes comments

--- utils/template.js
+++ utils/template.js
@@ -25,23 +25,17 @@ const create = (templateBluePrint) => {
     // schema to be used. Array types are special data types used in that module
     const fieldType = templateBluePrint[field].match(/\[.+\]/g) !== null ? 'array' : typeof templateBluePrint[field];
     if (fieldType === 'array') {
-      // Removes quotes from the array (example '[a, b]' ----> ['a','b'])
-      const removeArrayQuotes = (array) => array.replace(/\[|\]/g, '').replace(/,\s/g, ',').split(',');
-      let formattedValue = removeArrayQuotes(templateBluePrint[field]);
+      let formattedValue = templateBluePrint[field].replace(/\[|\]/g, '').replace(/,\s/g, ',').split(',');
 
-      // Deconstruct field into array and gets final value (example a.b.c ---> [a, b, c] ----> [a,b] c)
+      // Deconstruct field into array and gets final value
       const deconstructedField = field.split('.');
       const finalField = deconstructedField.pop();
 
-      // Check if the intended transformation is a.b.c, [d, e] ----> {a: {b: [{c: d}, {c: e}]}} or  {a: {b: {c: [d, e]}}}
       if (finalField.includes('{')) {
-        // Creates the array-value (example [{ id: v1 }, { id: v2 }]
         formattedValue = formattedValue.map(value => _.set({}, finalField.replace(/\{|\}/g, ''), value));
       }
-      // Reconstructs field
       const remainingField = deconstructedField.join('.');
 
-      // Puts everything together (a.b.c, [d, e] ----> { a: { b: { c: [d, e]}}} or a.b.{c}, [d, e] ----> { a: b: [{ c: d }, { c: e}] }
       _.set(template, remainingField, formattedValue);
     } else {
       _.set(template, field, templateBluePrint[field]);
