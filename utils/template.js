80904cdc387be24bc4c377abdaeefdfd877c5a57
PapadakisGeorge <george.papadakis@camelotls.com>
2021-10-13 12:51:33 +0300

    The create function now correctly handles arrays with objects as values.

--- utils/template.js
+++ utils/template.js
@@ -15,39 +15,39 @@ const blueprint = (
   };
 };
 
-const create = (templateBluePrint, extraJiraFields) => {
+const create = (templateBluePrint) => {
   const fieldKeys = Object.keys(templateBluePrint);
   const template = {};
   const extraFieldsAtomicView = {};
 
   fieldKeys.forEach((field) => {
-    let blueprintKey = templateBluePrint[field];
+    // For the extra fields supplied, we need to determine the data type in order to properly construct the JSON
+    // schema to be used. Array types are special data types used in that module
+    const fieldType = templateBluePrint[field].match(/\[.+\]/g) !== null ? 'array' : typeof templateBluePrint[field];
+    if (fieldType === 'array') {
+      // Removes quotes from the array (example '[a, b]' ----> ['a','b'])
+      const removeArrayQuotes = (array) => array.replace(/\[|\]/g, '').replace(/,\s/g, ',').split(',');
+      let formattedValue = removeArrayQuotes(templateBluePrint[field]);
 
-    if (Object.keys(extraJiraFields).includes(field)) {
-      // For the extra fields supplied, we need to determine the data type in order to properly construct the JSON
-      // schema to be used. Array types are special data types used in that module
-      const fieldType = templateBluePrint[field].match(/\[.+\]/g) !== null ? 'array' : typeof templateBluePrint[field];
+      // Deconstruct field into array and gets final value (example a.b.c ---> [a, b, c] ----> [a,b] c)
+      const deconstructedField = field.split('.');
+      const finalField = deconstructedField.pop();
 
-      if (fieldType === 'array') {
-        // TODO: The implementation below refers purely to cases that contain single element arrays since it's a hot fix
-        // for a last minute problem identified during testing. We should fix this logic to accept any length of arrays
-        // supplied.
-        const mainKey = `${field.replace('fields.', '')}`;
-        const parentArrayObject = { [mainKey]: [] };
-        const childKeyToBeQuoted = extraJiraFields[field].substring(extraJiraFields[field].indexOf('{') + 1, extraJiraFields[field].lastIndexOf(':'));
-        const childKeyValue = extraJiraFields[field].substring(extraJiraFields[field].indexOf(':') + 1, extraJiraFields[field].lastIndexOf('}')).trim();
-
-        parentArrayObject[mainKey].push({
-          [childKeyToBeQuoted]: childKeyValue
-        });
-
-        blueprintKey = parentArrayObject[mainKey];
+      // Check if the intended transformation is a.b.c, [d, e] ----> {a: {b: [{c: d}, {c: e}]}} or  {a: {b: {c: [d, e]}}}
+      if (finalField.includes('{')) {
+        // Creates the array-value (example [{ id: v1 }, { id: v2 }]
+        formattedValue = formattedValue.map(value => _.set({}, finalField.replace(/\{|\}/g, ''), value));
       }
-      _.set(extraFieldsAtomicView, field, { type: fieldType });
-    }
-    _.set(template, field, blueprintKey);
-  });
+      // Reconstructs field
+      const remainingField = deconstructedField.join('.');
 
+      // Puts everything together (a.b.c, [d, e] ----> { a: { b: { c: [d, e]}}} or a.b.{c}, [d, e] ----> { a: b: [{ c: d }, { c: e}] }
+      _.set(template, remainingField, formattedValue);
+    } else {
+      _.set(template, field, templateBluePrint[field]);
+    }
+    _.set(extraFieldsAtomicView, field, { type: fieldType });
+  });
   return { template, extraFieldsAtomicView };
 };
 
